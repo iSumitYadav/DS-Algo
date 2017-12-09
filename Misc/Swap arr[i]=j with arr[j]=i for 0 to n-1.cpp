7e566b5be40bf22954e15a6c323b3a3ab8298800
Sumit <sumityadavcools@gmail.com>
2017-12-10 00:31:35 +0530

    swap arr[i]=j & arr[j]=i New Method

--- Misc/Swap arr[i]=j with arr[j]=i for 0 to n-1.cpp
+++ Misc/Swap arr[i]=j with arr[j]=i for 0 to n-1.cpp
@@ -10,9 +10,9 @@ void swapIJUtil(int arr[], int n, int i){
 
 		arr[i] = val;
 
-		for(int j=0; j<n; j++)
+		/*for(int j=0; j<n; j++)
 			cout << arr[j] << " ";
-		cout << endl;
+		cout << endl;*/
 
 		val = -(i+1);
 		i = new_idx;
@@ -33,12 +33,44 @@ void swapIJ(int arr[], int n){
 		cout << arr[i] << " ";
 	}
 }
+ 
+void rearrangeIJElements(int arr[], int n){
+	for(int i=0; i<n; i++){
+		int new_i = abs(arr[i]);
+		int prev_i = i;
+		if(new_i == n)
+		    continue;
+
+		while(arr[new_i] >= 0){
+			int val_at_new_i = arr[new_i];
+			if(prev_i != 0)
+				arr[new_i] = -prev_i;
+			else
+				arr[new_i] = -n;
+			prev_i = new_i;
+			new_i = val_at_new_i;
+		}
+	}
+	
+	for(int i=0; i<n; i++){
+		if(arr[i] == -n)
+			arr[i] = 0;
+		else
+			arr[i] = -arr[i];
+		cout << arr[i] << " ";
+	}
+}
 
 int main(){
-	int arr[] = {3,2,4,7,8,1,0,6,5};//{2, 0, 1, 4, 5, 3};
+	int arr[] = {3, 2, 1, 0};//{0, 1, 2, 3};//{3,2,4,7,8,1,0,6,5};//{2, 0, 1, 4, 5, 3};
 	int n = sizeof(arr)/sizeof(arr[0]);
 
+	for(int i=0; i<n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 	swapIJ(arr, n);
+	cout << endl;
+	rearrangeIJElements(arr, n);
 
 	return 0;
 }
