3c4fbb119003e096c9c7f63219c2ffebaab7862d
Sumit <sumityadavcools@gmail.com>
2017-12-15 02:34:07 +0530

    Print Longest common subsequence

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -24,17 +24,37 @@ int lcs(char x[], char y[], int m, int n){
 		}cout << endl;
 	}*/
 
+	int idx = maxi;
+	char ans[idx];
+
+	i=m, j=n;
+	ans[idx--] = '\0';
+
+	while(i>0 && j>0){
+		if(dp[i][j] == dp[i-1][j]){
+			i--;
+		}else if(dp[i][j] == dp[i][j-1]){
+			j--;
+		}else if(dp[i][j] == dp[i-1][j-1]+1){
+			ans[idx--] = x[i-1];
+			i--;
+			j--;
+		}
+	}
+
+	cout << "LCS of " << x << " and " << y << " is: " << ans << endl;
+
 	return maxi;
 }
 
 int main(){
-	char x[] = "AGGTABY";
-	char y[] = "GXTXAYBY";
+	char x[] = "ABCDGH";
+	char y[] = "AEDFHR";
 
 	int m = strlen(x);
 	int n = strlen(y);
 
-	cout << "LCS of " << x << " and " << y << " is: " << lcs(x, y, m, n) << endl;
+	cout << "LCS of " << x << " and " << y << " is of length: " << lcs(x, y, m, n) << endl;
 
 	return 0;
 }
