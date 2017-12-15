cdf7789bd667d39d05b0fc02547f871d4a9a643d
Sumit <sumityadavcools@gmail.com>
2017-12-15 16:11:28 +0530

    Print LIS sequence | Printed Backwards

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -2,20 +2,30 @@
 using namespace std;
 
 int lis(int *arr, int n){
-	int dp[n], i, j, maxi = INT_MIN;;
+	int dp[n], i, j, maxi = INT_MIN, seq;
 	for(i=0; i<n; i++){
 		dp[i] = 1;
 	}
 
-	for(i=0; i<n; i++){
-		for(j=i+1; j<n; j++){
-			if(arr[j] > arr[i] && dp[j] < dp[i] + 1){
-				dp[j] = dp[i]+1;
-				if(maxi < dp[j])
-					maxi = dp[j];
+	for(i=1; i<n; i++){
+		for(j=0; j<i; j++){
+			if(arr[i] > arr[j] && dp[i] < dp[j] + 1){
+				dp[i] = dp[j]+1;
+				if(maxi < dp[i]){
+					seq = i;
+					maxi = dp[i];
+				}
 			}
 		}
 	}
+
+	cout << arr[seq] << " ";
+	for(i=seq-1; i>=0; i--){
+		if(dp[i] == dp[seq] - 1){
+			cout << arr[i] << " ";
+			seq = i;
+		}
+	}cout << endl;
 
 	return maxi;
 }
