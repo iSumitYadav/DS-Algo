a43bdd90df9db0e06bc590697dc3a35f3ea9b7a1
Sumit <sumityadavcools@gmail.com>
2017-12-15 17:46:05 +0530

    Minimum edit distance | Print the Operations done

--- DP/Minimum Edit distance.cpp
+++ DP/Minimum Edit distance.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int minEditDistance(char x[], char y[], int l1, int l2){
-	int dp[l1+1][l2+1], i, j;
+	int dp[l2+1][l1+1], i, j;
 
-	for(i=0; i<=l1; i++){
-		for(j=0; j<=l2; j++){
+	for(i=0; i<=l2; i++){
+		for(j=0; j<=l1; j++){
 			// cost to change(DELETE) all J characters of string2 to NULL string1(i==0) is j
 			if(i == 0)
 				dp[i][j] = j;
@@ -17,18 +17,47 @@ int minEditDistance(char x[], char y[], int l1, int l2){
 			// No cost incured if the character is same in both strings
 			// OR
 			// the cost is same as for a string of 1 character less
-			else if(x[i-1] == y[j-1])
+			else if(x[j-1] == y[i-1])
 				dp[i][j] = dp[i-1][j-1];
 
 			else{
 				dp[i][j] = 1 + min(min(dp[i-1][j], 		// Insert
-							dp[i][j-1]), 		// Delete
-							dp[i-1][j-1]); 		// Replace
+							dp[i][j-1]), 				// Delete
+							dp[i-1][j-1]); 				// Replace
 			}
 		}
 	}
 
-	return dp[l1][l2];
+	cout << x << endl << y << "\n\n";
+
+	/*for(i=1;i<=l2;i++){
+		for(j=1;j<=l1;j++){
+			cout << dp[i][j] << " ";
+		}cout << endl;
+	}cout << endl;*/
+
+	i = l2, j = l1;
+	while(i && j){
+		if(x[j-1] == y[i-1]){
+			i--;
+			j--;
+		}else if(dp[i][j] == 1 + dp[i-1][j]){
+			// Insert
+			cout << y[i-1] << " Inserted\n";
+			i--;
+		}else if(dp[i][j] == 1 + dp[i][j-1]){
+			// Delete
+			cout << x[j-1] << " Deleted\n";
+			j--;
+		}else if(dp[i][j] == 1 + dp[i-1][j-1]){
+			// Replace
+			cout << x[j-1] << " Replaced with " << y[i-1] << endl;
+			i--;
+			j--;
+		}
+	}cout << endl;
+
+	return dp[l2][l1];
 }
 
 int main(){
@@ -37,7 +66,7 @@ int main(){
 	int l1 = strlen(x);
 	int l2 = strlen(y);
 
-	cout << minEditDistance(x, y, l1, l2);
+	cout << "Min. edit distance is : " << minEditDistance(x, y, l1, l2);
 
 	return 0;
 }
