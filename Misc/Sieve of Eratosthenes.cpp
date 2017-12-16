fe8600fd8dcc2dbfd741e5bd996cb624e47d1df5
Sumit <sumityadavcools@gmail.com>
2017-12-16 22:42:46 +0530

    Sieve of Eratosthenes

---
+++ Misc/Sieve of Eratosthenes.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void generatePrime(int n){
+	int arr[n] = {0};
+
+	for(int i=2; i<=n; i++){
+		if(arr[i] != -1){
+			for(int j=2*i; j<=n; j+=i)
+				arr[j] = -1;
+		}
+	}
+
+	int i = 1;
+	while(i <= n){
+		if(arr[i] != -1)
+			cout << i << " ";
+		i++;
+	}
+}
+
+int main(){
+	int n;
+	cin >> n;
+
+	generatePrime(n);
+
+	return 0;
+}
