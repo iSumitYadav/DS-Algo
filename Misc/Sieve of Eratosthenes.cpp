4ed76de39bdd5cfeab1cc6d0e3111acd2881f6a9
Sumit <sumityadavcools@gmail.com>
2017-12-17 00:25:13 +0530

    typo fixed

--- Misc/Sieve of Eratosthenes.cpp
+++ Misc/Sieve of Eratosthenes.cpp
@@ -5,14 +5,14 @@ void generatePrime(int n){
 	int arr[n] = {0};
 
 	// for(int i=2; i<=n; i++){
-	for(int i=2; i*i<=n; i++) // Optimization
+	for(int i=2; i*i<=n; i++){ // Optimization
 		if(arr[i] != -1){
 			for(int j=2*i; j<=n; j+=i)
 				arr[j] = -1;
 		}
 	}
 
-	int i = 1;
+	int i = 2;
 	while(i <= n){
 		if(arr[i] != -1)
 			cout << i << " ";
