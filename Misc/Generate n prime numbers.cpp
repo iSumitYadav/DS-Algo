38e09c3bf2000ac4a0ad705114242cf8c4dbddb0
Sumit <sumityadavcools@gmail.com>
2017-12-18 14:44:56 +0530

    Generate first N prime numbers

---
+++ Misc/Generate n prime numbers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n, count=2, c, i = 3;
+	cin >> n;
+
+	if(n < 2)
+		cout << "No prime Numbers less than 2\n";
+	else{
+		cout << "2 ";
+
+		while(count <= n){
+			for(c=2; c<= i-1; c++){
+				if(i%c == 0)
+					break;
+			}
+
+			if(c == i){
+				cout << c << " ";
+				count++;
+			}
+			i++;
+		}
+	}
+
+	return 0;
+}
