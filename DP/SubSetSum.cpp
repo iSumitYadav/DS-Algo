dc184755da74cf5acbd668c8e979e58c43f10b1c
Sumit <sumityadavcools@gmail.com>
2017-12-20 15:29:50 +0530

    DP | Subset Sum problem | 0 sum condition fixed

--- DP/SubSetSum.cpp
+++ DP/SubSetSum.cpp
@@ -6,10 +6,10 @@ bool subSetSum(int arr[], int n, int sum){
 
 	for(int i=0; i<=n; i++){
 		for(int j=0; j<=sum; j++){
-			if(i == 0){
-				dp[i][j] = false;
-			}else if(j == 0)
-				dp[i][j] == true;
+			if(j == 0){
+				dp[i][j] = true;
+			}else if(i == 0)
+				dp[i][j] == false;
 			else if(j < arr[i-1]){
 				dp[i][j] = dp[i-1][j];
 			}else if(j >= arr[i-1]){
@@ -26,12 +26,13 @@ int main(){
 	int n = sizeof(arr)/sizeof(arr[0]);
 
 	int sum;
-	cin >> sum;
-
-	if(subSetSum(arr, n, sum))
-		printf("Yes\n");
-	else
-		printf("No\n");
+	
+	while(cin >> sum && sum > -1){
+		if(subSetSum(arr, n, sum))
+			printf("Yes\n");
+		else
+			printf("No\n");
+	}
 
 	return 0;
 }
