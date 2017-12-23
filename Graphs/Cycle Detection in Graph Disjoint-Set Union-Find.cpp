e7e0c057eb7350ad4478f20051ffec7e8e0ac91e
Sumit <sumityadavcools@gmail.com>
2017-12-23 18:07:13 +0530

    Graphs | Cycle Detection using Disjoint-Set Union-Find

---
+++ Graphs/Cycle Detection in Graph Disjoint-Set Union-Find.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+	int V, E;
+	list<int> *adj;
+	int *parent;
+
+public:
+	Graph(int v, int e){
+		this->V = v;
+		this->E = e;
+		adj = new list<int>[V];
+		parent = new int[V];
+		for(int i=0; i<V; i++)
+			parent[i] = -1;
+	}
+
+	void addEdge(int, int);
+	int findParent(int);
+	void unionSet(int, int);
+	bool isCyclic();
+};
+
+void Graph::addEdge(int u, int v){
+	adj[u].push_back(v);
+}
+
+int Graph::findParent(int i){
+	if(parent[i] == -1)
+		return i;
+
+	return findParent(parent[i]);
+}
+
+void Graph::unionSet(int a, int b){
+	int a_set = findParent(a);
+	int b_set = findParent(b);
+
+	parent[a_set] = b_set;
+}
+
+bool Graph::isCyclic(){
+	list<int>::iterator it;
+
+	for(int i=0; i<V; i++){
+		for(it=adj[i].begin(); it!=adj[i].end(); it++){
+			int x = findParent(i);
+			int y = findParent(*it);
+
+			if(x == y)
+				return true;
+
+			unionSet(x, y);
+		}
+	}
+
+	return false;
+}
+
+int main(){
+	Graph g(4, 3);
+
+	g.addEdge(0, 1);
+	g.addEdge(1, 2);
+	g.addEdge(0, 2);
+	g.addEdge(2, 3);
+	g.addEdge(3, 0);
+
+	printf("%s\n", g.isCyclic() ? "Cyclic" : "Not Cyclic");
+
+	return 0;
+}
