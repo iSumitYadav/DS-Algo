3a227f0d0aff6c144462aafe0171801b606ee66f
Sumit <sumityadavcools@gmail.com>
2017-12-25 23:02:26 +0530

    NumberTheory | Modular Exponentiation | O(log n) power method

--- Number Theory/Modular Exponentiation.cpp
+++ Number Theory/Modular Exponentiation.cpp
@@ -5,9 +5,22 @@ int modExpo(int x, int y, int p){
 	if(y == 0)
 		return 1;
 
-	int x_pow_y = x;
+	/*int x_pow_y = x;
 	for(int i=2; i<=y; i++)
-		x_pow_y = x_pow_y*x;
+		x_pow_y = x_pow_y*x;*/
+
+	int x_pow_y = 1;
+
+	// O(log y)
+	while(y > 0){
+		if(y & 1)
+			x_pow_y = x_pow_y * x;
+
+		y = y>>1; // y = y/2
+		x = x*x;
+	}
+
+	// cout<< x_pow_y << endl;
 
 	return x_pow_y % p;
 }
