4e4120ae765e7ee20ba07c0486766040db1bc995
Sumit <sumityadavcools@gmail.com>
2017-12-29 15:30:04 +0530

    BIT | Find elem that appear once | 2nd Method

--- BIT Manipulation/Find single occurence of number in array containing all other element thrice.cpp
+++ BIT Manipulation/Find single occurence of number in array containing all other element thrice.cpp
@@ -116,6 +116,16 @@ int findElement(int *arr, int n){
 	return ones;
 }
 
+/*
+We can sum the bits in same positions for all the numbers and take modulo with 3. The bits for which sum is not multiple of 3, are the bits of number with single occurrence.
+Let us consider the example array {5, 5, 5, 8}. The 101, 101, 101, 1000
+Sum of first bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+Sum of second bits%3 = (0 + 0 + 0 + 0)%0 = 0;
+Sum of third bits%3 = (1 + 1 + 1 + 0)%3 = 0;
+Sum of fourth bits%3 = (1)%3 = 1;
+Hence number which appears once is 1000
+*/
+
 #define INT_SIZE 32
 
 int findElement2(int *arr, int n){
@@ -130,6 +140,8 @@ int findElement2(int *arr, int n){
 				sum++;
 		}
 
+		/*The bits with sum not multiple of 3, are the
+      	bits of element with single occurrence*/
 		if(sum%3)
 			result = result | x;
 	}
